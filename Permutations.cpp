c4542602866ebb04a6d5c5827862d7a1e7d59111
varun sukalkar <92172651+varunsukalkar@users.noreply.github.com>
2025-05-13 09:34:43 +0000

    [46] [Time Beats: 30.59%] [Memory Beats: 87.61%] - LeetPush

--- Permutations.cpp
+++ Permutations.cpp
@@ -2,29 +2,17 @@ class Solution {
 public:
 
       
-
-    void permut(vector<int> arr , vector<vector<int>>&ans,vector<int>  & temp,
-\tvector<bool>&visited){
-\tif(arr.size()==temp.size()){
-\t\t// for(int i = 0; i< temp.size(); i++){
-\t\t// \tcout<<temp[i]<<\ \;
-\t\t// }
-\t\t// cout<<endl;
-        ans.push_back(temp);
-\treturn ;\t
-\t}
-\tfor(int i = 0; i< arr.size(); i++){
-\t\tif(visited[i]==0){
-\t\t\tvisited[i]=1;
-\t\t\ttemp.push_back(arr[i]);
-\t\t\tpermut(arr,ans,temp,visited);
-\t\t\tvisited[i]=0;
-\t\t\ttemp.pop_back();
-\t\t}
-\t}
-}
-
-
+      void cheack(int index , vector<int>& nums, vector<vector<int>>& v , int n){
+        if(n<=index){
+            v.push_back(nums);
+            return ; 
+        }
+        for(int i = index ; i< n; i++){
+            swap(nums[i],nums[index]);
+            cheack(index+1 , nums,v,n);
+            swap(nums[i],nums[index]);
+        }
+      }
 
 
 
@@ -32,11 +20,10 @@ public:
 
 
     vector<vector<int>> permute(vector<int>& nums) {
-          vector<vector<int>> ans;
-          vector<int>temp;
-          vector<bool>visited(nums.size(),0);
-          permut(nums,ans,temp,visited);
-          return ans;
+       vector<vector<int>>v;
+       int n = nums.size();
+       cheack(0  , nums,v,n);
+       return v;
 
     }
 };
